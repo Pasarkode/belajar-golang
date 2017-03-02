1eff805efebaf8c924c907e9067ae97d17f3367f
Taufik R. Firdaus <taufik_ramadan@hotmail.com>
2017-03-02 09:46:19 +0700

    First commit.

---
+++ halo.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//di golang tidak membutuhkan semicolon aka ;(titik koma) sebagai penutup baris
+	fmt.Println("Hallo dunia...")
+}
---
+++ variable.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//integer untuk bilangan bulat
+	var i int
+	//float untuk bilangan pecahan atau desimal
+	var f float64
+	//string digunakan untuk variable yang berisi angka dan huruf
+	var s string
+	//variable integer tidak memerlukan tanda kutip("")
+	i = 10
+	//didalam pemrograman tanda koma(,) digantikan dengan tanda titik(.)
+	f = 3.14
+	//untuk variable string dibutuhkan tanda kutip("")
+	s = "Saya Taufik R. Firdaus"
+	//untuk menampilkan isi variable tidak memerlukan tanda kutip("")
+	fmt.Println(i)
+
+	fmt.Println(f)
+	//untuk menampilkan dua variable dalam satu kali keluaran digunakan tanda koma(,)
+	fmt.Println(i, f)
+
+	fmt.Println("Halo dunia... ",s)
+}
