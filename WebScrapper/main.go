f2f4ea4ba4d406557757b8d53991dbad303ed5d3
Hyugen <taufik_ramadan@hotmail.com>
2017-05-25 17:55:15 +0700

    fix bug

--- WebScrapper/main.go
+++ WebScrapper/main.go
@@ -36,8 +36,28 @@ func checkErr(err error, msg string) {
 	}
 }
 
+func parse(url string) {
+	res, err := soup.Get(url)
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	doc := soup.HTMLParse(res)
+
+	getList := doc.Find("ul", "class", "g_mod_bd content-list").FindAll("a")
+
+	for _, link := range getList {
+
+		url := "https:"+link.Attrs()["href"]
+
+		go ParseNovel(url)
+	}
+
+}
+
 func ParseNovel(url string) {
-	//var dbmap = InitDB()
+	var dbmap = InitDB()
 	res, err := soup.Get(url)
 
 	if err != nil {
@@ -51,76 +71,35 @@ func ParseNovel(url string) {
 	getContent := getNovel.Find("div", "class", "cha-content").FindAll("p")
 
 	for _, title := range getTitle{
-		//for _, content := range getContent{
 		chaTit := strings.Split(title.Text(), ": ")
-		/*chap := chaTit[0]
-		tit := chaTit[1]*/
-		//novel := strings.Join(getContent, "")
-		fmt.Println(chaTit)
+		chap := chaTit[0]
+		tit := chaTit[1]
+		fmt.Println(tit)
 		var temp []string
 		for _, content := range getContent {
 			temp = append(temp, "<p>"+content.Text()+"</p>")
 		}
-		fmt.Println("__________________________________________________________________")
-		fmt.Print(strings.Join(temp, ""))
-		/*novels := Novels{
+		novels := Novels{
 			Chapter:chap,
 			Title:tit,
-			Content:,
-		}*/
-
-		//err := dbmap.Insert(&novels)
-		//checkErr(err, "Gagal memasukkan novel")
-		//}
-	}
-
-}
-
-func parse(url string) {
-	res, err := soup.Get(url)
-
-	if err != nil {
-		os.Exit(1)
-	}
-
-	doc := soup.HTMLParse(res)
-
-	getList := doc.Find("ul", "class", "g_mod_bd content-list").FindAll("a")
-
-	go func() {
-		for _, link := range getList {
-
-			getChapter, err := soup.Get("https:"+link.Attrs()["href"])
-
-			if err != nil {
-				os.Exit(1)
-			}
-
-			body := soup.HTMLParse(getChapter)
-
-			go func() {
-				pages := body.Find("div", "class", "cha-page j_contentWrap")
-
-				titles := pages.Find("div", "class", "cha-tit").FindAll("h3")
-
-				for _, title := range titles{
-					fmt.Println(title.Text()+" Link: https:"+link.Attrs()["href"])
-				}
-			}()
-
+			Content:strings.Join(temp, ""),
 		}
-	}()
+
+		err := dbmap.Insert(&novels)
+		checkErr(err, "Gagal memasukkan novel")
+		fmt.Println("Done!")
+	}
+
 }
 
 func main() {
 
 	var url,text string
 	//url = "https://www.webnovel.com/book/7176992105000305"
-	fmt.Println("Masukkan URL dari Webnovel.com")
+	fmt.Print("Masukkan URL halaman TOC dari https://webnovel.com: ")
 	fmt.Scanln(&url)
-	//go parse(url)
-	go ParseNovel(url)
-	defer fmt.Println("Scrapping Novel The King's Avatar")
+	go parse(url)
+	//go ParseNovel(url)
 	fmt.Scanln(&text)
 
 }
